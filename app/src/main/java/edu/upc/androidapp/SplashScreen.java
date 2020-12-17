521b27463ca4822eb618b195e18a3fd3dbb0bedb
mxapelli <72257485+mxapelli@users.noreply.github.com>
2020-12-17 20:51:28 +0100

    Update SplashScreen.java

--- app/src/main/java/edu/upc/androidapp/SplashScreen.java
+++ app/src/main/java/edu/upc/androidapp/SplashScreen.java
@@ -2,7 +2,9 @@ package edu.upc.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+            }
+        },3000);
+        
     }
 }
