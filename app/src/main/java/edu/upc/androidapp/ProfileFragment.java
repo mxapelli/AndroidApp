bb55dd77e4bb45bd09ad726f26205df2983b12a5
mxapelli <72257485+mxapelli@users.noreply.github.com>
2020-12-18 20:07:12 +0100

    Menu actualizado
    Falta arreglar logout

--- app/src/main/java/edu/upc/androidapp/ProfileFragment.java
+++ app/src/main/java/edu/upc/androidapp/ProfileFragment.java
@@ -1,5 +1,8 @@
 package edu.upc.androidapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +13,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
+    private static final String MY_PREFS_NAME = "user_pass_pref";
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container,false);
+
     }
+
 }
